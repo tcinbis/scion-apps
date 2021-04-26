628af17fc8c2e08dd98f618204fa8943ea72329d
Jonas Gessner <JonasGessner@users.noreply.github.com>
2021-04-26 12:46:55 +0200

    Add connection-oriented echo example

---
+++ _examples/echo/echo.go
@@ -0,0 +1,132 @@
+// Copyright 2018 ETH Zurich
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"time"
+
+	"github.com/netsec-ethz/scion-apps/pkg/pan"
+)
+
+func main() {
+	var err error
+	// get local and remote addresses from program arguments:
+	port := flag.Uint("port", 0, "[Server] local port to listen on")
+	remoteAddr := flag.String("remote", "", "[Client] Remote (i.e. the server's) SCION Address (e.g. 17-ffaa:1:1,[127.0.0.1]:12345)")
+	flag.Parse()
+
+	if (*port > 0) == (len(*remoteAddr) > 0) {
+		check(fmt.Errorf("Either specify -port for server or -remote for client"))
+	}
+
+	if *port > 0 {
+		err = runServer(int(*port))
+		check(err)
+	} else {
+		for {
+			err = runClient(*remoteAddr)
+			check(err)
+		}
+	}
+}
+
+func runServer(port int) error {
+	listener, err := pan.ListenUDP(context.Background(), &net.UDPAddr{Port: port}, nil)
+	if err != nil {
+		return err
+	}
+	defer listener.Close()
+	fmt.Println(listener.LocalAddr())
+
+	buffer := make([]byte, 16*1024)
+	knownConnections := make(map[net.Addr]pan.Conn)
+	for {
+		n, from, err := listener.ReadFrom(buffer)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		data := buffer[:n]
+		conn := knownConnections[from]
+		if conn == nil {
+			conn, err = listener.MakeConnectionToRemote(context.Background(), from.(pan.UDPAddr), nil, nil)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			knownConnections[from] = conn
+			fmt.Printf("Received from new connection %s: %s\n", from, data)
+		} else {
+			fmt.Printf("Received from known connection %s: %s\n", from, data)
+		}
+		
+		msg := fmt.Sprintf("take it back! %s", time.Now().Format("15:04:05.0"))
+		n, err = conn.Write([]byte(msg))
+		fmt.Printf("Wrote %d bytes.\n", n)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+	}
+}
+
+func runClient(address string) error {
+	addr, err := pan.ParseUDPAddr(address)
+	if err != nil {
+		return err
+	}
+	conn, err := pan.DialUDP(context.Background(), nil, addr, nil, nil)
+	if err != nil {
+		return err
+	}
+	fmt.Println("err", err)
+	defer conn.Close()
+
+	go func() {
+		buffer := make([]byte, 16*1024)
+		for {
+			n, err := conn.Read(buffer)
+			if err != nil {
+				fmt.Println(err)
+				continue
+			}
+			data := buffer[:n]
+			fmt.Printf("Received reply: %s\n", data)
+		}
+	}()
+	for i := 0; i < 10; i++ {
+		//nBytes, err := conn.Write([]byte(fmt.Sprintf("hello world %s", time.Now().Format("15:04:05.0"))))
+		nBytes, err := conn.Write([]byte(fmt.Sprintf("hello world %s", time.Now().Format("15:04:05.0"))))
+		if err != nil {
+			return err
+		}
+		fmt.Printf("Wrote %d bytes.\n", nBytes)
+		time.Sleep(1 * time.Second)
+	}
+	return nil
+}
+
+// Check just ensures the error is nil, or complains and quits
+func check(e error) {
+	if e != nil {
+		fmt.Fprintln(os.Stderr, "Fatal error:", e)
+		os.Exit(1)
+	}
+}
