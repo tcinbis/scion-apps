f1c5bfba8efbda5e2cc282e6ddc7a2a2a4ba57c2
tcinbis <tcinbis@gmail.com>
2021-05-23 22:29:02 +0200

    Extend shttp server API with quic config parameter

--- pkg/shttp/server.go
+++ pkg/shttp/server.go
@@ -17,6 +17,7 @@ package shttp
 import (
 	"context"
 	"crypto/tls"
+	"github.com/lucas-clemente/quic-go"
 	"fmt"
 	"net"
 	"net/http"
@@ -33,7 +34,7 @@ type Server struct {
 
 // ListenAndServe listens for HTTPS connections on the SCION address addr and calls Serve
 // with handler to handle requests
-func ListenAndServe(addr string, handler http.Handler, tlsConfig *tls.Config) error {
+func ListenAndServe(addr string, handler http.Handler, tlsConfig *tls.Config, quicCfg *quic.Config) error {
 
 	scionServer := &Server{
 		Server: &http3.Server{
@@ -41,6 +42,7 @@ func ListenAndServe(addr string, handler http.Handler, tlsConfig *tls.Config) er
 				Addr:    addr,
 				Handler: handler,
 			},
+			QuicConfig: quicCfg,
 		},
 	}
 	scionServer.TLSConfig = tlsConfig
@@ -49,13 +51,14 @@ func ListenAndServe(addr string, handler http.Handler, tlsConfig *tls.Config) er
 
 // Serve creates a listener on conn and listens for HTTPS connections.
 // A new goroutine handles each request using handler
-func Serve(conn net.PacketConn, handler http.Handler) error {
+func Serve(conn net.PacketConn, handler http.Handler, quicCfg *quic.Config) error {
 
 	scionServer := &Server{
 		Server: &http3.Server{
 			Server: &http.Server{
 				Handler: handler,
 			},
+			QuicConfig: quicCfg,
 		},
 	}
 
